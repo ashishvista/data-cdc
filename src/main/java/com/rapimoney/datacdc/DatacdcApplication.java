85e67d05fa06d19cdb34d372f28d1bf699d60d26
ashish <ashish.kumar@rapimoney.com>
2023-08-19 09:36:05 +0530

    first commit

---
+++ src/main/java/com/rapimoney/datacdc/DatacdcApplication.java
@@ -0,0 +1,13 @@
+package com.rapimoney.datacdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rapimoney/datacdc/DatacdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapimoney.datacdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatacdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
